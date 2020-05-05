1ecbe3774f643a3d78fe4d9cc0d09cccbc6fef75
Philippe Beck <philippe@philippebeck.net>
2020-05-05 18:15:31 +0200

    Renaming & refactoring methods

--- Animadio.js
+++ Animadio.js
@@ -2,14 +2,14 @@
 
 class Animadio {
   /**
-   * @param {Object} input
+   * @param {Object} inputs
    * @param {string} key
    * @param {Object} duration
    */
-  constructor(input, key, duration = [5000, {}, {}]) {
-    this.inputIds   = input;
-    this.inputCount = input.length;
-    this.input      = [];
+  constructor(inputs, key, duration = [5000, {}, {}]) {
+    this.inputIds   = inputs;
+    this.inputCount = inputs.length;
+    this.inputs     = [];
 
     this.duration       = duration[0];
     this.durationBase   = duration[0];
@@ -20,80 +20,98 @@ class Animadio {
     this.hub    = document.getElementById(key + "-hub");
     this.goal   = document.getElementById(key + "-goal");
 
-    this.getValues();
+    this.getAllValues();
     this.check.addEventListener("click", this.clickCheckBtn.bind(this));
   }
 
-  getValues() {
+  getAllValues() {
     for (let i = 0; i < this.inputCount; i++) {
-      this.input[i] = document.getElementById(this.inputIds[i]);
-      this.input[i].addEventListener("input", this.getValue.bind(this, i));
+      this.inputs[i] = document.getElementById(this.inputIds[i]);
+      this.inputs[i].addEventListener("input", this.getValue.bind(this, i));
     }
   }
 
   /**
-   * @param {number} i
+   * @param {number} index
    * @returns {string}
    */
-  getValue(i) {
-    return this.input[i].value;
+  getValue(index) {
+    return this.inputs[index].value;
   }
 
   clickCheckBtn() {
     this.check.setAttribute("disabled", true);
-    this.addClasses();
+    this.AddAllClasses();
   }
 
-  /**
-   * @param {number} i
-   */
-  setDuration(i) {
-    if (this.durationValues) {
-      for (let [key, value] of Object.entries(this.durationValues)) {
-        if (this.input[i].value.includes(key)) {
-          this.duration = value;
-        }
-      }
-    }
-  }
-
-  /**
-   * @param {number} i
-   */
-  setCount(i) {
-    if (this.countValues) {
-      for (let [key, value] of Object.entries(this.countValues)) {
-        if (this.input[i].value.includes(key)) {
-          this.duration = this.duration * value;
-        }
-      }
-    }
-  }
-
-  addClasses() {
+  AddAllClasses() {
     for (let i = 0; i < this.inputCount; i++) {
 
-      if (this.input[i].value) {
-        this.check.classList.add(this.input[i].value + "-check");
-        this.hub.classList.add(this.input[i].value + "-hub");
-        this.goal.classList.add(this.input[i].value + "-goal");
+      if (this.inputs[i].value) {
+        this.addClass(i);
       }
 
-      this.setDuration(i);
-      this.setCount(i);
+      if (this.durationValues) {
+        this.setDuration(i);
+      }
+
+      if (this.countValues) {
+        this.setCount(i);
+      }
     }
-    window.setTimeout(this.removeClasses.bind(this), this.duration);
+    window.setTimeout(this.removeAllClasses.bind(this), this.duration);
   }
 
-  removeClasses() {
+  /**
+   * @param {number} index
+   */
+  addClass(index) {
+    this.check.classList.add(this.inputs[index].value + "-check");
+    this.hub.classList.add(this.inputs[index].value + "-hub");
+    this.goal.classList.add(this.inputs[index].value + "-goal");
+  }
+
+  /**
+   * @param {number} index
+   */
+  setDuration(index) {
+    for (let [key, value] of Object.entries(this.durationValues)) {
+      if (this.inputs[index].value.includes(key)) {
+        this.duration = value;
+      }
+    }
+  }
+
+  /**
+   * @param {number} index
+   */
+  setCount(index) {
+    for (let [key, value] of Object.entries(this.countValues)) {
+      if (this.inputs[index].value.includes(key)) {
+        this.duration = this.duration * value;
+      }
+    }
+  }
+
+  removeAllClasses() {
     this.check.checked  = false;
     this.duration       = this.durationBase;
 
     for (let i = 0; i < this.inputCount; i++) {
-      this.check.classList.remove(this.input[i].value + "-check");
-      this.hub.classList.remove(this.input[i].value + "-hub");
-      this.goal.classList.remove(this.input[i].value + "-goal");
+
+      if (this.inputs[i].value) {
+        this.removeClass(i);
+      }
     }
     this.check.removeAttribute("disabled");
+  }
+
+  /**
+   * @param {number} index
+   */
+  removeClass(index) {
+    this.check.classList.remove(this.inputs[index].value + "-check");
+    this.hub.classList.remove(this.inputs[index].value + "-hub");
+    this.goal.classList.remove(this.inputs[index].value + "-goal");
   }
 }
