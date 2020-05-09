2efd02ad332b4646c76ea0007fbc7fbe5870394d
Philippe Beck <philippe@philippebeck.net>
2020-05-09 22:36:13 +0200

    Version 0.1.6

--- dist/animadio.js
+++ dist/animadio.js
@@ -11,25 +11,6 @@ class Animadio {
   getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-  /**
-   * @param selector
-   * @return
-   */
-  getElement(selector) {
-    return document.querySelector(selector);
-  }
-
-  /**
-   * @param selector
-   * @param type
-   * @param listener
-   */
-  installEventListener(selector, type, listener) {
-    var element;
-    element = document.querySelector(selector);
-    element.addEventListener(type, listener);
-  }
 }
 
 class Input extends Animadio {
@@ -64,14 +45,14 @@ class Input extends Animadio {
 
   getAllElements() {
     for (let elementIndex = 0; elementIndex < this.elementCount; elementIndex++) {
-      this.classes[elementIndex] = this.getElement("#" + this.mainElement + "-" + this.elements[elementIndex]);
+      this.classes[elementIndex] = document.querySelector("#" + this.mainElement + "-" + this.elements[elementIndex]);
     }
     this.mainClass = this.classes[0];
   }
 
   getAllValues() {
     for (let inputIndex = 0; inputIndex < this.inputCount; inputIndex++) {
-      this.inputs[inputIndex] = this.getElement("#" + this.inputIds[inputIndex]);
+      this.inputs[inputIndex] = document.querySelector("#" + this.inputIds[inputIndex]);
       this.inputs[inputIndex].addEventListener("input", this.getValue.bind(this, inputIndex));
     }
   }
@@ -162,104 +143,140 @@ class Input extends Animadio {
 }
 
 class Slider extends Animadio {
-  constructor(slides) {
+  constructor() {
     super();
 
-    this.slides = slides;
-    this.state  = {index: -1, timer: window.setInterval(this.nextSlide.bind(this), 5000)};
+    this.index = 0;
 
-    this.installEventListener("#slider-random", "click", this.randomSlide.bind(this));
-    this.installEventListener("#slider-previous", "click", this.previousSlide.bind(this));
-    this.installEventListener("#slider-next", "click", this.nextSlide.bind(this));
-    this.installEventListener("#slider-toggle", "click", this.autoSlide.bind(this));
-    this.installEventListener("#toolbar-toggle", "click", this.showToolbar.bind(this));
+    this.slider   = document.getElementById("slider");
+    this.relay    = document.getElementById("slider-relay");
+
+    this.autoTrigger    = document.getElementById("slider-check-auto");
+    this.repeatTrigger  = document.getElementById("slider-check-repeat");
+    this.randomTrigger  = document.getElementById("slider-check-random");
+
+    this.auto   = document.getElementById("slider-auto");
+    this.repeat = document.getElementById("slider-repeat");
+    this.random = document.getElementById("slider-random");
+
+    this.previous = document.getElementById("slider-previous");
+    this.next     = document.getElementById("slider-next");
+
+    this.allTriggers    = this.slider.querySelectorAll("input");
+    this.slidesCount    = this.relay.querySelectorAll("figure").length;
+    this.slidesTriggers = [];
+
+    this.getTriggers();
+    this.setControls();
+
+    this.timer = window.setInterval(this.nextSlide.bind(this), 2000);
   }
 
-  nextSlide() {
-    this.state.index++;
-
-    if (this.state.index === this.slides.length) {
-      this.state.index = 0;
-    }
-    this.refreshSlide();
-  }
-
-  previousSlide() {
-    this.state.index--;
-
-    if (this.state.index < 0) {
-      this.state.index = this.slides.length - 1;
-    }
-    this.refreshSlide();
-  }
-
-  randomSlide() {
-    let index;
-
-    do {
-      index = this.getRandomInteger(0, this.slides.length - 1);
-    }
-    while (index === this.state.index);
-
-    this.state.index = index;
-
-    this.refreshSlide();
-  }
-
-  autoSlide() {
-    let icon;
-    let toggle;
-
-    icon    = this.getElement("#slider-toggle i");
-    toggle  = this.getElement("#slider-toggle");
-
-    icon.classList.toggle("fa-play");
-    icon.classList.toggle("fa-pause");
-
-    if (this.state.timer == null) {
-      this.state.timer = window.setInterval(this.nextSlide.bind(this), 5000);
-
-      toggle.title = "Pause";
-    } else {
-      window.clearInterval(this.state.timer);
-
-      this.state.timer  = null;
-      toggle.title      = "Play";
+  getTriggers() {
+    let triggerCount = 0;
+    for (let triggerIndex = 0; triggerIndex < this.allTriggers.length; triggerIndex++) {
+      if (this.allTriggers[triggerIndex].getAttribute("type") === "radio") {
+        this.slidesTriggers[triggerCount] = this.allTriggers[triggerIndex];
+        triggerCount++;
+      }
     }
   }
 
-  showToolbar() {
-    let icon;
-    let toggle;
-    let nav;
+  setControls() {
+    this.previous.addEventListener("click", this.previousSlide.bind(this));
+    this.repeat.addEventListener("click", this.repeatSlide.bind(this));
+    this.autoTrigger.addEventListener("click", this.autoSlide.bind(this));
+    this.random.addEventListener("click", this.randomSlide.bind(this));
+    this.next.addEventListener("click", this.nextSlide.bind(this));
 
-    icon    = this.getElement("#toolbar-toggle i");
-    toggle  = this.getElement("#toolbar-toggle");
-    nav     = this.getElement(".slider-nav ul");
-
-    icon.classList.toggle("fa-toggle-on");
-    icon.classList.toggle("fa-toggle-off");
-
-    nav.classList.toggle("none");
-
-    if (nav.classList.contains("none")) {
-      toggle.title = "Open the Slider Toolbar";
-    } else {
-      toggle.title = "Close the Slider Toolbar";
-    }
+    document.addEventListener("keydown", this.keyboardControls.bind(this));
   }
 
   refreshSlide() {
-    let sliderImage;
-    let sliderLegend;
+    let previousIndex = this.index - 1;
 
-    sliderImage  = this.getElement("#slider img");
-    sliderLegend = this.getElement("#slider figcaption");
+    if (previousIndex < 0) {
+      previousIndex = this.slidesCount - 1;
+    }
+    if (previousIndex === this.slidesCount) {
+      previousIndex = 0;
+    }
 
-    sliderImage.src          = this.slides[this.state.index].image;
-    sliderLegend.textContent = this.slides[this.state.index].legend;
+    this.slidesTriggers[this.index].setAttribute("checked", true);
+    this.slidesTriggers[previousIndex].removeAttribute("checked");
+  }
+
+  previousSlide() {
+    this.index--;
+
+    if (this.index < 0) {
+      this.index = this.slidesCount - 1;
+    }
+
+    this.refreshSlide();
+  }
+
+  repeatSlide() {
+    this.repeatTrigger.toggleAttribute("checked");
+  }
+
+  autoSlide() {
+    this.autoTrigger.toggleAttribute("checked");
+
+    if (this.timer === null) {
+      this.timer        = window.setInterval(this.nextSlide.bind(this), 2000);
+      this.auto.title   = "Pause";
+
+    } else {
+      window.clearInterval(this.timer);
+      this.timer        = null;
+      this.auto.title   = "Play";
+    }
+  }
+
+  randomSlide() {
+    this.randomTrigger.toggleAttribute("checked");
+
+    if (this.random.getAttribute("checked" === true)) {
+      this.index = this.getRandomInteger(0, this.slidesCount - 1);
+    }
+
+    this.refreshSlide();
+  }
+
+  nextSlide() {
+    this.index++;
+
+    if (this.index >= this.slidesCount) {
+      this.index = 0;
+    }
+
+    this.refreshSlide();
+  }
+
+  /**
+   * @param {Object} event
+   */
+  keyboardControls(event) {
+    switch (event.code) {
+      case "ArrowLeft":
+        this.previousSlide();
+        break;
+      case "ArrowUp":
+        this.repeatSlide();
+        break;
+      case "Space":
+        this.autoSlide();
+        break;
+      case "ArrowDown":
+        this.randomSlide();
+        break;
+      case "ArrowRight":
+        this.nextSlide();
+        break;
+    }
   }
 }
 
 /*! Author: Philippe Beck <philippe@philippebeck.net>
- Updated: 6th May 2020 @ 9:18:59 PM */
+ Updated: 9th May 2020 @ 10:32:19 PM */
