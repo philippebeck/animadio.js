550829b8521b173c119953a59ba9f3d0d1e851b1
Philippe Beck <philippe@philippebeck.net>
2020-05-10 15:10:07 +0200

    Add getRandomInteger to Slider class

--- src/Slider.js
+++ src/Slider.js
@@ -47,6 +47,15 @@ class Slider extends Animadio {
     }
   }
 
+  /**
+   * @param min
+   * @param max
+   * @return
+   */
+  getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   refreshSlide() {
     for (let slidesIndex = 0; slidesIndex < this.slidesCount; slidesIndex++) {
       if (this.slidesTriggers[slidesIndex].hasAttribute("checked")) {
