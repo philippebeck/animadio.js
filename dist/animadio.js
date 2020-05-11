20d3cdff2ab5a1c4156f0de65a2df21a2e0504b2
Philippe Beck <philippe@philippebeck.net>
2020-05-11 21:13:07 +0200

    Version 0.1.11

--- dist/animadio.js
+++ dist/animadio.js
@@ -1,15 +1,19 @@
-/*! animadio.js v0.1.10 | https://animadio.org | MIT License */
+/*! animadio.js v0.1.11 | https://animadio.org | MIT License */
 
 "use strict";
 
 class Animadio {
 
-  static input (inputs, elements = ["animadio", ["trigger"]], duration = [2000, {}, {}]) {
+  static input(inputs, elements = ["animadio", ["trigger"]], duration = [2000, {}, {}]) {
     new Input(inputs, elements, duration);
   }
 
-  static slider (timeout = 2000, auto = true, random = false) {
+  static slider(timeout = 2000, auto = true, random = false) {
     new Slider(timeout, auto, random);
+  }
+
+  static canvas(width = 500, height = 500, line = 2, color = "navy") {
+    new Canvas(width, height, line, color);
   }
 }
 
@@ -327,5 +331,139 @@ class Slider {
   }
 }
 
+class Canvas {
+  /**
+   * @param {number} width
+   * @param {number} height
+   * @param {number} line
+   * @param {string} color
+   */
+  constructor(width = 500, height = 500, line = 2, color = "navy") {
+    this.startState = false;
+    this.endState   = false;
+
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.touchX = 0;
+    this.touchY = 0;
+
+    this.canvas   = document.getElementById("canvas");
+    this.context  = this.canvas.getContext("2d");
+    this.cleaner  = document.getElementById("canvas-cleaner");
+
+    this.initCanvas(width, height);
+    this.initContext(line, color);
+    this.initEvents();
+  }
+
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  initCanvas(width, height) {
+    this.canvas.width   = width;
+    this.canvas.height  = height;
+  }
+
+  /**
+   * @param {number} line
+   * @param {string} color
+   */
+  initContext(line, color) {
+    this.context.lineWidth    = line;
+    this.context.strokeStyle  = color;
+    this.context.lineCap      = "round";
+    this.context.lineJoin     = "round";
+  }
+
+  initEvents() {
+    this.canvas.addEventListener("mousedown", this.moveWithMouse.bind(this));
+    this.canvas.addEventListener("mousemove", this.drawWithMouse.bind(this));
+    this.canvas.addEventListener("mouseup", this.stopDrawing.bind(this));
+    this.canvas.addEventListener("mouseout", this.stopDrawing.bind(this));
+
+    this.canvas.addEventListener("touchstart", this.moveWithTouch.bind(this));
+    this.canvas.addEventListener("touchmove", this.drawWithTouch.bind(this));
+    this.canvas.addEventListener("touchend", this.stopDrawing.bind(this));
+    this.canvas.addEventListener("touchcancel", this.stopDrawing.bind(this));
+
+    this.cleaner.addEventListener("click", this.cleanCanvas.bind(this));
+  }
+
+  startDrawing() {
+    this.startState = true;
+    this.context.beginPath();
+  }
+
+  stopDrawing() {
+    this.startState = false;
+    this.endState   = true;
+  }
+
+  cleanCanvas() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.endState = false;
+  }
+
+  /**
+   * @param {object} event
+   */
+  getMouseLocation(event) {
+    this.mouseX = event.offsetX;
+    this.mouseY = event.offsetY;
+  }
+
+  /**
+   * @param {object} event
+   */
+  getTouchLocation(event) {
+    let position  = event.target.getBoundingClientRect();
+    this.touchX   = event.targetTouches[0].clientX - position.left;
+    this.touchY   = event.targetTouches[0].clientY - position.top;
+  }
+
+  /**
+   * @param {object} event
+   */
+  moveWithMouse(event) {
+    this.startDrawing();
+    this.getMouseLocation(event);
+    this.context.moveTo(this.mouseX, this.mouseY);
+    event.preventDefault();
+  }
+
+  /**
+   * @param {object} event
+   */
+  moveWithTouch(event) {
+    this.startDrawing();
+    this.getTouchLocation(event);
+    this.context.moveTo(this.touchX, this.touchY);
+    event.preventDefault();
+  }
+
+  /**
+   * @param {object} event
+   */
+  drawWithMouse(event) {
+    if (this.startState) {
+      this.getMouseLocation(event);
+      this.context.lineTo(this.mouseX, this.mouseY);
+      this.context.stroke();
+    }
+  }
+
+  /**
+   * @param {object} event
+   */
+  drawWithTouch(event) {
+    if (this.startState) {
+      this.getTouchLocation(event);
+      this.context.lineTo(this.touchX, this.touchY);
+      this.context.stroke();
+    }
+  }
+}
+
 /*! Author: Philippe Beck <philippe@philippebeck.net>
- Updated: 10th May 2020 @ 4:45:29 PM */
+ Updated: 11th May 2020 @ 9:05:13 PM */
