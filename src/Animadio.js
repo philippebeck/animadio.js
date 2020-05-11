26652b990fe794fa30bb4097bae5f274f85d4082
Philippe Beck <philippe@philippebeck.net>
2020-05-11 21:12:37 +0200

    Add static canvas

--- src/Animadio.js
+++ src/Animadio.js
@@ -2,11 +2,15 @@
 
 class Animadio {
 
-  static input (inputs, elements = ["animadio", ["trigger"]], duration = [2000, {}, {}]) {
+  static input(inputs, elements = ["animadio", ["trigger"]], duration = [2000, {}, {}]) {
     new Input(inputs, elements, duration);
   }
 
-  static slider (timeout = 2000, auto = true, random = false) {
+  static slider(timeout = 2000, auto = true, random = false) {
     new Slider(timeout, auto, random);
+  }
+
+  static canvas(width = 500, height = 500, line = 2, color = "navy") {
+    new Canvas(width, height, line, color);
   }
 }
